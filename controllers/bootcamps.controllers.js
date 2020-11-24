5c249c9197f1748ee2c6c53367fad8365711d3cd
Nitin Ranganath <nitinranganath@gmail.com>
2020-11-24 23:36:10 +0530

    Update fetch all bootcamps endpoint to return count as well

--- controllers/bootcamps.controllers.js
+++ controllers/bootcamps.controllers.js
@@ -6,7 +6,9 @@ const Bootcamp = require('../models/Bootcamp.model');
 exports.getBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await Bootcamp.find();
-    res.status(200).json({ success: true, data: bootcamps });
+    res
+      .status(200)
+      .json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (err) {
     res.status(400).json({ success: false });
   }
