6cd6da4b025bbcfe05969604bffc20667a1b9509
Nitin Ranganath <nitinranganath@gmail.com>
2020-11-25 00:38:13 +0530

    Implement mongoose middleware for generating slug using slugify

--- models/Bootcamp.model.js
+++ models/Bootcamp.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const bootcampSchema = new mongoose.Schema(
   {
@@ -96,5 +97,11 @@ const bootcampSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Create bootcamp slug from name
+bootcampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 module.exports = mongoose.model('Bootcamp', bootcampSchema);
