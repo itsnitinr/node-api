a609a31df952f31d435eb1fbce98849da7ab280f
Nitin Ranganath <nitinranganath@gmail.com>
2020-11-25 00:52:49 +0530

    Implement mongoose middleware for generating GeoJSON data using node-geocoder

--- models/Bootcamp.model.js
+++ models/Bootcamp.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder.utils');
 
 const bootcampSchema = new mongoose.Schema(
   {
@@ -101,6 +102,23 @@ const bootcampSchema = new mongoose.Schema(
 // Create bootcamp slug from name
 bootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode and create location field
+bootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+  this.address = undefined;
   next();
 });
 
