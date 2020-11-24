4cc3daf27a28912e5b9cf85144bee3ce4d7f4a2c
Nitin Ranganath <nitinranganath@gmail.com>
2020-11-24 23:22:52 +0530

    Add enpoint for fetching all bootcamps and single bootcamp by ID

--- controllers/bootcamps.controllers.js
+++ controllers/bootcamps.controllers.js
@@ -3,17 +3,28 @@ const Bootcamp = require('../models/Bootcamp.model');
 // @route   GET /api/v1/bootcamps
 // @desc    Get all bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @route   GET /api/v1/bootcamps/:id
 // @desc    Get a bootcamp
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @route   POST /api/v1/bootcamps
