a1d52582c6f0cb1edd578a07fc50b26d38a8c27e
Nitin Ranganath <nitinranganath@gmail.com>
2020-11-29 02:26:47 +0530

    Add JWT to cookie while sending token response

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -16,10 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // Create JWT token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 // @route   POST /api/v1/auth/login
@@ -45,8 +42,26 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
-  // Create JWT token
+  sendTokenResponse(user, 200, res);
+});
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({ success: true, token });
+};
