7e6c48688619bb3162b85c39d9e6a20b1c76e378
Nitin Ranganath <nitinranganath@gmail.com>
2020-12-04 22:18:46 +0530

    Implement endpoint for updating logged in user details

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -56,6 +56,23 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: user });
 });
 
+// @route   PUT /api/v1/auth/updatedetails
+// @desc    Update details of logged in user
+// @access  Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: user });
+});
+
 // @route   POST /api/v1/auth/forgotpassword
 // @desc    Forgot Password
 // @access  Public
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,6 +7,7 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
 } = require('../controllers/auth.controllers');
 
 const { protect } = require('../middleware/auth.middleware');
@@ -14,6 +15,7 @@ const { protect } = require('../middleware/auth.middleware');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resetToken', resetPassword);
 
